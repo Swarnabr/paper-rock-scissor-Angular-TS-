c79ca04994c35d290ed1fe6c94a2d9b4df4c34da
Swarnabr <164309954+Swarnabr@users.noreply.github.com>
2024-03-25 20:33:48 +0530

    paper rock scissor(Angular TS)

---
+++ PRS/source code/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Rou2Component } from './rou2/rou2.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Rou2Component,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
